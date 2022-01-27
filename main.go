c2e7930dfca57e2f489406c40a2d1e12134ec171
Jeremy Brudvik <jeremy@brudvik.me>
2022-01-27 12:59:44 -0500

    First attempt -- broken

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+)
+
+/*
+TODO:
+- Get correct metrics
+- Ensure consistent core ordering (these shouldn't be sorted...)
+*/
 
 func main() {
-    fmt.Println("cpus: ? ? ? ? ? ? ? ?")
+	PrintPercents()
+	// PrintCores()
+}
+
+func PrintPercents() {
+    percents, err := cpu.Percent(0, true)
+	if err != nil {
+		fmt.Println("Could not get CPUs")
+	}
+	for _, percent := range percents {
+		fmt.Printf("%0.1f%% ", percent)
+	}
+	fmt.Println()
+}
+
+func PrintCores() {
+    counts, err := cpu.Counts(true)
+	if err != nil {
+		fmt.Println("Could not get cores")
+	}
+	fmt.Println("Cores:", counts)
 }
